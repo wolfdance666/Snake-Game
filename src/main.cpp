61792952d830ff9607820d71021ca162c4f1ce01
radroman <radu.roman@here.com>
2017-12-08 13:09:38 +0100

    Removed text mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <ncurses.h>
 #include <SFML/Graphics.hpp>
 #include <chrono>
 #include <iostream>
@@ -8,104 +7,10 @@
 #include "Game.h"
 #include "Playground.h"
 #include "PlaygroundGraphicView.h"
-#include "PlaygroundView.h"
 #include "Point.h"
 #include "Snake.h"
-#include "Window.h"
 
 using namespace std;
-void textMode()
-{
-    initscr(); /* Start curses mode */
-    cbreak();
-    noecho();
-    nodelay(stdscr, true);
-    refresh();
-    curs_set(0);
-    keypad(stdscr, true);
-
-    srand(static_cast<unsigned int>(time(nullptr)));
-
-    Playground playground(48, 23);
-    Snake snake(playground, Point(25, 12));
-    FoodGenerator foodgenerator(playground);
-
-    Window scoreWin(Point(50, 5), Point(1, 1));
-
-    const Point size = Point(50, 25);
-    Window gameWin(size, Point(1, 6));
-
-    PlaygroundView playgroundView(playground, gameWin, Point(1, 1));
-
-    bool gameIsRunning = true;
-    bool gameIsPaused = false;
-    while (gameIsRunning)
-    {
-        int ch = getch();
-        gameWin.clear();
-        gameWin.printBorder();
-        switch (ch)
-        {
-        case KEY_LEFT:
-            snake.setDirection(Snake::LEFT);
-            break;
-        case KEY_RIGHT:
-            snake.setDirection(Snake::RIGHT);
-            break;
-        case KEY_UP:
-            snake.setDirection(Snake::UP);
-            break;
-        case KEY_DOWN:
-            snake.setDirection(Snake::DOWN);
-            break;
-        case 'g':
-            snake.grow();
-            break;
-        case 'q':
-            gameIsRunning = false;
-            break;
-        case 'p':
-            gameIsPaused = !gameIsPaused;
-            break;
-        case 'n':
-            if (snake.isDead())
-            {
-                playground.reset();
-                snake.reset(Point(25, 12));
-            }
-            break;
-        case KEY_RESIZE:
-            gameWin.resize(size);
-            break;
-        case ERR:
-            break;
-        }
-        if (!gameIsPaused)
-        {
-            foodgenerator.update();
-            snake.move();
-            playgroundView.render();
-        }
-        else
-        {
-            gameWin.printString(Point(5, 12), "THE GAME IS PAUSED. PRESS P TO CONTINUE");
-        }
-
-        if (snake.isDead())
-        {
-            gameWin.printString(Point(20, 12), "GAME OVER!!!");
-        }
-        gameWin.refresh();
-
-        scoreWin.clear();
-        scoreWin.printBorder();
-        scoreWin.printString(Point(5, 2), "SCORE: " + std::to_string(snake.getSize() - 1));
-        scoreWin.refresh();
-
-        this_thread::sleep_for(chrono::milliseconds(100));
-    }
-    endwin(); /* End curses mode */
-}
 
 void graphicMode()
 {
@@ -113,9 +18,6 @@ void graphicMode()
     sf::RenderWindow window(sf::VideoMode(800, 800), "Snake Game");
     window.setVerticalSyncEnabled(true);
 
-//    Playground playground(50, 50);
-//    Snake snake(playground, Point(25, 12));
-//    FoodGenerator foodgenerator(playground);
     Game snakeGame(50, 50);
     PlaygroundGraphicView graphicView(snakeGame.getPlayground());
     graphicView.setPosition(sf::Vector2f(10, 50));
@@ -193,21 +95,8 @@ void graphicMode()
 }
 int main()
 {
-//    char c;
-//    cout << "Text/Graphic?(t/g)" << endl;
-//    cin >> c;
-//    if (c == 't')
-//    {
-//        textMode();
-//    }
-//    else if (c == 'g')
-//    {
+
         graphicMode();
-//    }
-//    else
-//    {
-//        cout << "Invalid input (expected t or g)" << endl;
-//        return 1;
-//    }
+
     return 0;
 }
